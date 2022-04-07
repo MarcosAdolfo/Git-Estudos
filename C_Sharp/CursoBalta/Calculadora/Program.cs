6b5110b6d1c214b7aae6216ffa0f6ea7134e6a63
MarcosAdolfo <marcosadolfopbastos@hotmail.com>
2022-04-07 16:47:35 -0300

    Primeiras Aulas

--- C_Sharp/CursoBalta/Calculadora/Program.cs
+++ C_Sharp/CursoBalta/Calculadora/Program.cs
@@ -1,2 +1,48 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace Calculadora
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Soma();
+            Subitracao();
+        }
+
+        static void Soma()
+        {
+            Console.Clear();
+
+            Console.Write("Primeiro Valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.Write("Segundo Valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float total = v1 + v2;
+            Console.WriteLine($"A soma de {v1} + {v2} = {total}");
+        }
+
+        static void Subitracao()
+        {
+            Console.Clear();
+
+            Console.Write("Primeiro Valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.Write("Segundo Numero: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float total = v1 - v2;
+            Console.WriteLine($"A subitração de {v1} - {v2} = {total}");
+        }
+    }
+
+}
