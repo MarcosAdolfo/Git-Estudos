23183731abcb8d097564893f25cad58ba7ec127d
MarcosAdolfo <marcosadolfopbastos@hotmail.com>
2022-04-14 12:52:30 -0300

    finalizando

--- C_Sharp/CursoBalta/Calculadora/Program.cs
+++ C_Sharp/CursoBalta/Calculadora/Program.cs
@@ -8,8 +8,31 @@ namespace Calculadora
     {
         static void Main(string[] args)
         {
-            // Soma();
-            Subitracao();
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Escolha um opção:");
+            Console.WriteLine("1 - Soma\n2 - Subtração\n3 - Divisão\n4 - Multiplicação\n5 - Sair");
+
+            Console.WriteLine("--------------------");
+
+            Console.Write("Opção: ");
+            short res = short.Parse(Console.ReadLine());
+
+            switch(res)
+            {
+                //case 0:Soma();break;
+                case 1:Soma();break;
+                case 2:Subtracao();break;
+                case 3:Divisao();break;
+                case 4:Multiplicacao();break;
+                case 5:System.Environment.Exit(0); break;
+                default: Menu();break;
+            }
         }
 
         static void Soma()
@@ -26,9 +49,11 @@ namespace Calculadora
 
             float total = v1 + v2;
             Console.WriteLine($"A soma de {v1} + {v2} = {total}");
+            Console.ReadKey();
+            Menu();
         }
 
-        static void Subitracao()
+        static void Subtracao()
         {
             Console.Clear();
 
@@ -42,6 +67,44 @@ namespace Calculadora
 
             float total = v1 - v2;
             Console.WriteLine($"A subitração de {v1} - {v2} = {total}");
+            Console.ReadKey();
+            Menu();
+        }
+
+        static void Divisao()
+        {
+            Console.Clear();
+
+            Console.Write("Primeiro Valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.Write("Segundo Numero: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float total = v1 / v2;
+            Console.WriteLine($"A Divisão de {v1} / {v2} = {total}");
+            Console.ReadKey();
+            Menu();
+        }
+
+        static void Multiplicacao()
+        {
+            Console.Clear();
+
+            Console.Write("Primeiro Valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.Write("Segundo Numero: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float total = v1 * v2;
+            Console.WriteLine($"A Multiplicação de {v1} * {v2} = {total}");
+            Console.ReadKey();
+            Menu();
         }
     }
 
