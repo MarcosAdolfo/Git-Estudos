e73572a71167e793947c76430c593d742aa12590
MarcosAdolfo <marcosadolfopbastos@hotmail.com>
2022-05-04 14:27:01 -0300

    Incremento da minha parte.

--- C_Sharp/CursoBalta/Calculadora/Program.cs
+++ C_Sharp/CursoBalta/Calculadora/Program.cs
@@ -16,7 +16,7 @@ namespace Calculadora
             Console.Clear();
 
             Console.WriteLine("Escolha um opção:");
-            Console.WriteLine("1 - Soma\n2 - Subtração\n3 - Divisão\n4 - Multiplicação\n5 - Sair");
+            Console.WriteLine("1 - Soma\n2 - Subtração\n3 - Divisão\n4 - Multiplicação\n5 - Regra De Três\n0 - Sair");
 
             Console.WriteLine("--------------------");
 
@@ -25,12 +25,12 @@ namespace Calculadora
 
             switch(res)
             {
-                //case 0:Soma();break;
+                case 0:Console.Clear(); System.Environment.Exit(0); break;
                 case 1:Soma();break;
                 case 2:Subtracao();break;
                 case 3:Divisao();break;
                 case 4:Multiplicacao();break;
-                case 5:Console.Clear(); System.Environment.Exit(0); break;
+                case 5:RegraDeTres(); break;
                 default: Menu();break;
             }
         }
@@ -103,6 +103,40 @@ namespace Calculadora
 
             float total = v1 * v2;
             Console.WriteLine($"A Multiplicação de {v1} * {v2} = {total}");
+            Console.ReadKey();
+            Menu();
+        }
+
+        //Incremento da minha parte.
+
+        static void RegraDeTres()
+        {
+            Console.Clear();
+
+            Console.WriteLine("x1 ———— y1");
+            Console.WriteLine("x2 ———— y2");
+
+            Console.WriteLine("");
+
+            Console.Write("Valor de x1: ");
+            float x1 = float.Parse(Console.ReadLine());
+
+            Console.Write("Valor de y1: ");
+            float y1 = float.Parse(Console.ReadLine());
+
+            Console.Write("Valor de x2: ");
+            float x2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            Console.WriteLine($"{x1} ———— {y1}");
+            Console.WriteLine($"{x2} ———— y2");
+
+            Console.WriteLine("");
+
+            float y2 = (x2 * y1) / x1;
+
+            Console.WriteLine("y2 = {0}",y2);
             Console.ReadKey();
             Menu();
         }
