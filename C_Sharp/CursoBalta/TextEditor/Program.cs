f0bb8611f4bd93ceddc7c10e25fbcc6213c4a627
MarcosAdolfo <marcosadolfopbastos@hotmail.com>
2022-05-10 16:54:19 -0300

    Edito de Texto

--- C_Sharp/CursoBalta/TextEditor/Program.cs
+++ C_Sharp/CursoBalta/TextEditor/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace TextEditor
 {
@@ -21,14 +22,59 @@ namespace TextEditor
             {
                 case 0: System.Environment.Exit(0); break;
                 case 1: Abrir(); break;
-                case 2: Criar(); break;
+                case 2: Editar(); break;
                 default: Menu(); break;
             }
         }
 
-        static void Abrir(){}
+        static void Abrir()
+        {
+            Console.Clear();
+            Console.WriteLine("Qual o caminho do arquivo?");
+            string path = Console.ReadLine();
 
-        static void Criar(){}
+            using(var file = new StreamReader(path))
+            {
+                string text = file.ReadToEnd();
+                Console.WriteLine(text);
+            }
+            
+            Console.ReadLine();
+            Menu();
+        }
+
+        static void Editar()
+        {
+            Console.Clear();
+            Console.WriteLine("Digite seu texto; - (ESC Para sair)");
+            Console.WriteLine("_________________________________________");
+            string text = "";
+
+            do
+            {
+                text += " " + Console.ReadLine();
+                text += Environment.NewLine;
+            }
+            while(Console.ReadKey().Key != ConsoleKey.Escape);
+
+            Salvar(text);
+        }
+
+        static void Salvar(string text)
+        {
+            Console.Clear();
+            Console.WriteLine("Qual caminho para salver o arquivo?");
+            var path = Console.ReadLine();
+
+            using(var file = new StreamWriter(path))
+            {
+                file.Write(text);
+            }
+
+            Console.WriteLine($"Arquivo {path} Salvo Com Sucesso!");
+            Console.ReadKey();
+            Menu();
+        }
     }
 
 }
