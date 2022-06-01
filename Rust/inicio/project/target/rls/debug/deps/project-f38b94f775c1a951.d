63c195857801f3934685f884277860a19bd81b62
MarcosAdolfo <marcosadolfopbastos@hotmail.com>
2022-06-01 13:13:24 -0300

    rust

---
+++ Rust/inicio/project/target/rls/debug/deps/project-f38b94f775c1a951.d
@@ -0,0 +1,5 @@
+d:\Documentos\Git-Estudos\Rust\inicio\project\target\rls\debug\deps\project-f38b94f775c1a951.rmeta: src\main.rs
+
+d:\Documentos\Git-Estudos\Rust\inicio\project\target\rls\debug\deps\project-f38b94f775c1a951.d: src\main.rs
+
+src\main.rs:
---
+++ Rust/inicio/project/target/rls/debug/deps/project-fbfe233a41325861.d
@@ -0,0 +1,5 @@
+d:\Documentos\Git-Estudos\Rust\inicio\project\target\rls\debug\deps\project-fbfe233a41325861.rmeta: src\main.rs
+
+d:\Documentos\Git-Estudos\Rust\inicio\project\target\rls\debug\deps\project-fbfe233a41325861.d: src\main.rs
+
+src\main.rs:
