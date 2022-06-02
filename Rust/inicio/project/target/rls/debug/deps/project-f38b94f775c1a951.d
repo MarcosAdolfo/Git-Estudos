2f891a9cf9641d2c22cc9f4702122592c56edcef
MarcosAdolfo <marcosadolfopbastos@hotmail.com>
2022-06-02 09:41:32 -0300

    RustF

--- Rust/inicio/project/target/rls/debug/deps/project-f38b94f775c1a951.d
+++
@@ -1,5 +0,0 @@
-d:\Documentos\Git-Estudos\Rust\inicio\project\target\rls\debug\deps\project-f38b94f775c1a951.rmeta: src\main.rs
-
-d:\Documentos\Git-Estudos\Rust\inicio\project\target\rls\debug\deps\project-f38b94f775c1a951.d: src\main.rs
-
-src\main.rs:
