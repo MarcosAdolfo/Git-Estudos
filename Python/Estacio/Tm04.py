8332cb7548108bda50af6e8fb4217761c7d5f719
MarcosAdolfo <92380430+MarcosAdolfo@users.noreply.github.com>
2022-10-25 17:01:43 -0300

    Tm04_02

--- Python/Estacio/Tm04.py
+++ Python/Estacio/Tm04.py
@@ -1,6 +1,23 @@
 class Carro():
-    def __init__(self, name):
+    def __init__(self, name, valor):
         self.nome = name;
+        self.valor = valor;
 
-carro1 = Carro("BMW");
-print(carro1.nome)
+    def Total(self):
+        return self.valor.Preco()
+    
+
+
+class Valor():
+    def __init__(self,valor,imposto, descontos):
+        self.valor = valor;
+        self.imposto = imposto;
+        self.descontos = descontos;
+
+    def Preco(self):
+        return self.valor + self.imposto - self.descontos;
+
+
+preco = Valor(16000,1076,300)
+carro1 = Carro("BMW", preco);
+print(carro1.nome, carro1.Total)
